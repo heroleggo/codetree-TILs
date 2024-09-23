1c4486b2c490d9f57e9e70703ea22ac24744ee41
Youngwoong You <h3r0.y0u@gmail.com>
2024-09-23 23:37:39 +0900

    Title: 함수를 이용한 소수 판별, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240923/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,17 @@
+import math
+
+def isPrime(n):
+    for i in range(2, math.ceil(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+[a, b] = [int(x) for x in input().split()]
+
+result = 0
+
+for i in range(a, b + 1):
+    if isPrime(i):
+        result += i
+
+print(result)
