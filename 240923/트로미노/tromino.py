cae20eda0a14b049c7bd94beb6d34cf24b0e7dc2
Youngwoong You <h3r0.y0u@gmail.com>
2024-09-23 23:17:01 +0900

    Title: 트로미노, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 240923/트로미노/tromino.py
+++ 240923/트로미노/tromino.py
@@ -9,7 +9,7 @@ result = -1
 
 # check pattern 1
 for i in range(0, n - 1):
-    for j in range(0, n - 1):
+    for j in range(0, m - 1):
         values = [board[i][j], board[i + 1][j], board[i][j + 1], board[i + 1][j + 1]]
         values.sort()
         result = max(result, sum(values) - min(values))
@@ -17,12 +17,12 @@ for i in range(0, n - 1):
 
 # check pattern 2 - 1
 for i in range(0, n - 2):
-    for j in range(0, n):
+    for j in range(0, m):
         result = max(result, board[i][j] + board[i + 1][j] + board[i + 2][j])
 
 # check pattern 2 - 2
 for i in range(0, n):
-    for j in range(0, n - 2):
+    for j in range(0, m - 2):
         result = max(result, board[i][j] + board[i][j + 1] + board[i][j + 2])
 
 print(result)
