a1a232f138dad784dc85902251466169ba9d38f0
Youngwoong You <h3r0.y0u@gmail.com>
2024-10-03 23:14:42 +0900

    Title: 퀵 정렬 구현, Time: 5079ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 241003/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,33 @@
+def quick(start, end):
+    global data
+    if start < end:
+        # select pivot (mid)
+        pivot = partition(start, end)
+
+        quick(start, pivot - 1)
+        quick(pivot + 1, end)
+
+def partition(left, right):
+    global data
+    pivot = left
+    i = left + 1
+    j = right - 1
+    
+    while i <= j:
+        while data[i] < data[pivot]:
+            i += 1
+        while data[j] > data[pivot]:
+            j -= 1
+        if i <= j:
+            # swap
+            data[i], data[j] = data[j], data[i]
+    data[pivot], data[j] = data[j], data[pivot]
+    return j
+
+n = int(input())
+
+data = list(map(int, input().split()))
+
+quick(0, len(data))
+
+print(' '.join([str(x) for x in data]))
