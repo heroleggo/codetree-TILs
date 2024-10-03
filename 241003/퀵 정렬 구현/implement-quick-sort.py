ee2e2ab151b50db8d91561cb286a762b721aadeb
Youngwoong You <h3r0.y0u@gmail.com>
2024-10-03 23:17:13 +0900

    Title: 퀵 정렬 구현, Time: 5043ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 241003/퀵 정렬 구현/implement-quick-sort.py
+++ 241003/퀵 정렬 구현/implement-quick-sort.py
@@ -22,7 +22,7 @@ def partition(left, right):
             # swap
             data[i], data[j] = data[j], data[i]
     data[pivot], data[j] = data[j], data[pivot]
-    return j
+    return j + 1
 
 n = int(input())
 
